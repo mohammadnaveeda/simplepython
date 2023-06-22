5fadb086c61b6a7c0e38e174c85e8b2b4b084b2b
Naveed <naveedamindfire@gmail.com>
2023-06-22 18:36:06 +0530

    my first commit

---
+++ app1.py
@@ -0,0 +1,18 @@
+#App that converts Feet into Metres
+print("Ft to Mt Convertor App")
+ 
+#Python function to make it done.
+
+def feet_to_meters(feet):
+    meters = feet * 0.3048
+    return meters
+
+# Prompt the user to enter a value in feet
+feet_input = float(input("Enter a value in feet: "))
+
+# Call the feet_to_meters function and store the result
+meters_result = feet_to_meters(feet_input)
+
+# Print the result
+print(f"{feet_input} feet is equal to {meters_result} meters.")
+ 
