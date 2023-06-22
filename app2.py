520c96cb14b2f69a0fff620c864f9697fda1b969
Naveed <naveedamindfire@gmail.com>
2023-06-22 20:02:36 +0530

    added if else statement in app2.py

--- app2.py
+++ app2.py
@@ -1,3 +1,12 @@
 #File 2
 print("Thank you for viewing the first file. That app works perfectly")
 print("Someday I will be also become a great Hacker")
+
+#Marks if else statements
+marks = float(input("Enter the marks: "))
+
+if marks >= 60:
+    print("Passed")
+else:
+    print("Failed")
+
